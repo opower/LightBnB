fd3a128094a1bdeec376354bb1f1af9dc6fee0f2
Olivia <oliviapower.02@gmail.com>
2020-02-06 21:50:55 +0000

    update file structure

---
+++ LightBnB_WebApp-master/db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+module.exports = {
+  pool
+}
