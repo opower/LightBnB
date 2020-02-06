6e19e3f48434528f5288acf602bfddbd14261787
Olivia <oliviapower.02@gmail.com>
2020-02-06 02:42:34 +0000

    get all reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -60,7 +60,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  return pool.query(`SELECT properties.*, reservations.*, AVG(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN reservations ON properties.id = property_id
+  JOIN property_reviews ON reservations.id = reservation_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then(res => res.rows)
+  
 }
 exports.getAllReservations = getAllReservations;
 
